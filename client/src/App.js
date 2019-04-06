69c4bbd20055378f0718ed55f88efa882421c9da
david-chua <dp_chua@hotmail.com>
2019-04-06 13:46:07 -0400

    login workin in client

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { Route, NavLink, withRouter } from 'react-router-dom';
 import './App.css';
 
+
+import Login from './login/Login';
+
 class App extends Component {
   render() {
     return (
@@ -11,11 +14,12 @@ class App extends Component {
             <NavLink to="/register">Sign Up</NavLink>
             &nbsp; | &nbsp;
             <NavLink to="/login">Login</NavLink>
-            &nbsp; | &nbsp;  
+            &nbsp; | &nbsp;
             <NavLink to="/users">Jokes</NavLink>
           </nav>
         </header>
         <main>
+          <Route path="/login" component={ Login } />
 
         </main>
       </div>
