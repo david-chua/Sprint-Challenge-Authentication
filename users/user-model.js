47f5e910b7df6a0579f48e50077c9f015270bc70
david-chua <dp_chua@hotmail.com>
2019-04-06 13:07:46 -0400

    login created

--- users/user-model.js
+++ users/user-model.js
@@ -3,12 +3,17 @@ const db = require('../database/dbConfig.js');
 module.exports = {
   add,
   find,
+  findBy,
   findById,
 };
 
 
 function find() {
   return db('users').select('id', 'username', 'password');
+}
+
+function findBy(filter) {
+  return db('users').where(filter);
 }
 
 async function add(user) {
